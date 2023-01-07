57a19e45deab0cdc38bebc4bb53bca0479f0088c
Sit-Ko-Naing <sitkonaing.esrc@gmail.com>
2023-01-07 22:15:01 +0630

    PJ_section-1

---
+++ template/footer.php
@@ -0,0 +1,6 @@
+<P>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sequi earum nisi voluptatibus excepturi iusto, vero cum reprehenderit dolores consequatur. In, consequatur? Atque, distinctio eveniet repellat aliquam quae iste dolorem?</P>
+
+</body>
+
+<script src="./assets/js/app.js"> </script>
+</html>
